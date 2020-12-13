e3dd819d05e2d5e0d3f0b6b3b8e2ef9aba4e914f
nosideri <madi.stollmeyer@gmail.com>
2020-12-12 21:34:09 -0800

    added thought controller and models

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -10,5 +10,19 @@ const thoughtController = {
             res.json(results)
         })
     },
-    
+    createThought: function(req, res) {
+        db.Thought.create().then(function(results){
+            res.json(results)
+        })
+    },
+    updateThought: function(req, res) {
+        db.Thought.updateOne({_id: req.params.id}).then(function(results){
+            res.json(results)
+        })
+    },
+    deleteThought: function(req, res) {
+        db.Thought.deleteOne({_id: req.params.id}).then(function(results){
+            res.json(results)
+        })
+    }
 }
